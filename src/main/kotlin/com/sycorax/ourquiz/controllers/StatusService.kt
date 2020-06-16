95348a6e90d205e0e958b597d667644999354a2a
Bethany Drake <bethanyd@thoughtworks.com>
2020-06-16 14:10:41 +1000

    dont break if you get status of a question that doesn't exist

--- src/main/kotlin/com/sycorax/ourquiz/controllers/StatusService.kt
+++ src/main/kotlin/com/sycorax/ourquiz/controllers/StatusService.kt
@@ -6,9 +6,10 @@ data class StatusResponse(val questionNumber: Int, val revealed: Boolean)
 class StatusService {
 
     val NOT_STARTED: StatusResponse = StatusResponse(-1, false)
-
+    val FINISHED: StatusResponse = StatusResponse(-1, true)
     fun getStatus(quiz: Quiz): StatusResponse{
         if (!quiz.hasStarted) return NOT_STARTED
+        if( quiz.currentQuestion == quiz.questions.size) return FINISHED
         val revealed = quiz.questions[quiz.currentQuestion].revealed
         return  StatusResponse(quiz.currentQuestion, revealed)
     }
